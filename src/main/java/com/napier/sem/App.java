518f2df8448e5501ce3c2ff9a57f0bbc5a5d036b
Sam Al-Khamis <40438221@live.napier.ac.uk>
2022-02-17 13:57:15 +0000

    Employee Class implemented

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -5,6 +5,7 @@ import java.sql.*;
 public class App
 {
 
+
     public static void main(String[] args)
     {
         // Create new Application
@@ -15,6 +16,7 @@ public class App
 
         // Disconnect from database
         a.disconnect();
+
     }
 
 
